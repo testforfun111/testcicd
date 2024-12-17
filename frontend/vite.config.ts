0a2f98c508085ae021058e3ba86dfc04f108b63b
testforfun111 <93455188+testforfun111@users.noreply.github.com>
2024-12-17 10:05:58 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002, 
+    port: 5173, 
     proxy: {
       '/api/v1': {
         target: 'http://localhost:8081',
